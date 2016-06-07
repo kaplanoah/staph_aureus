61fae42e95726a2cecd78b631d9ec91618cc6877
Noah Kaplan <noahkaplan4@gmail.com>
2016-06-07 01:30:52 -0400

    clean genome

--- clean_genome.py
+++ clean_genome.py
@@ -1,18 +1,20 @@
 import re
 
-genome = ''
-
-numbered_line_with_bases = re.compile(r'^( +)\d+ [a-z ]+$')
-invalid_bases = re.compile(r'[^acgtrykmswbdhvn]')
+numbered_line_with_bases     = re.compile(r'^( +)\d+ [a-zA-Z ]+$')
+expected_non_base_characters = re.compile(r'[ 0-9\n]')
+not_valid_base               = re.compile(r'[^acgtrykmswbdhvn]')
 
 with open('staph_aureus_genome_raw.txt',   'r') as raw_genome_file, \
      open('staph_aureus_genome_clean.txt', 'w') as clean_genome_file:
 
     for line in raw_genome_file:
-        if re.match(numbered_line_with_bases, line):
 
-            bases = re.sub(invalid_bases, '', line)
+        if not re.match(numbered_line_with_bases, line):
+            continue
 
-            if len(bases) != 60:
-                print 'WARNING: unexpected number of valid bases'
-                print line.strip()
+        bases = re.sub(expected_non_base_characters, '', line)
+
+        if re.search(not_valid_base, bases):
+            raise Exception('Invalid base: %s' % line.strip())
+
+        clean_genome_file.write(bases)
